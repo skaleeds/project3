0aa12f396cd6c5b27e79e94d3a39740f4898abc2
skaleeds <skaleel@hawk.iit.edu>
2023-11-24 20:10:08 -0600

    css and js
    changes to js and css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-s
+document.addEventListener('DOMContentLoaded', function(){
+	if(document.body.classList.contains('index-page')){
+		
+		const pageTitle = document.getElementById('page-title');
+		const hiddenElement = document.getElementById('hidden-element');
+		pageTitle.addEventListener('click', function (){
+			hiddenElement.classList.toggle('hidden');
+		});	
+	}
+});
