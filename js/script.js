9b458e98a2ec4751290eb3ae65882947ba40687e
skaleeds <skaleel@hawk.iit.edu>
2023-11-26 07:41:31 -0600

    Update script.js
    check to see if clicked is giving a response

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
 	if(document.body.classList.contains('index-page')){
-		
 		const pageTitle = document.getElementById('page-title');
 		const hiddenElement = document.getElementById('hidden-element');
 		pageTitle.addEventListener('click', function (){
+			console.log('clicked');
 			hiddenElement.classList.toggle('hidden');
 		});	
 	}
