9df96ae1bd93794d122eb3fabbb4c17d42821592
skaleeds <skaleel@hawk.iit.edu>
2023-11-26 07:48:53 -0600

    js info fix
    fixing issue of info not showing

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
 	if(document.body.classList.contains('index-page')){
-		const pageTitle = document.getElementById('page-title');
+		const pageTitle = document.querySelector('.float-left h3');
 		const hiddenElement = document.getElementById('hidden-element');
 		pageTitle.addEventListener('click', function (){
-			console.log('clicked');
 			hiddenElement.classList.toggle('hidden');
 		});	
 	}
