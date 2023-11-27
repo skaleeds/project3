215e34edc9bd483a2c2f7e918439f864cf438a88
skaleeds <skaleel@hawk.iit.edu>
2023-11-27 06:47:07 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
-	if(document.body.classList.contains('index-page')){
-		const pageTitle = document.querySelectorAll('h3[data-hidden-element-id]');
-		
-	    pageTitle.forEach(currentTitle => {
-			const hiddenElementId = currentTitle.getAttribute('data-hidden-element-id');
-			const hiddenElement = document.getElementById(hiddenElementId);
-			currentTitle.addEventListener('click', function (){
-				hiddenElement.classList.toggle('hidden');
-			}};
-		});
-	}
+    if(document.body.classList.contains('index-page')){
+        const pageTitle = document.querySelectorAll('h3[data-hidden-element-id]');
+        
+        pageTitle.forEach(title => {
+            const hiddenElementId = title.getAttribute('data-hidden-element-id');
+            const hiddenElement = document.getElementById(hiddenElementId);
+            title.addEventListener('click', function (){
+                hiddenElement.classList.toggle('hidden');
+            });
+        });
+    }
 });
