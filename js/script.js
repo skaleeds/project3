9fe2507700daa659d737f7b8c9187cd9deb52117
skaleeds <skaleel@hawk.iit.edu>
2023-11-27 06:13:42 -0600

    Update script.js
    fix to get all working when clicked

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
 	if(document.body.classList.contains('index-page')){
-		const pageTitle = document.querySelectorAll('.float-left h3');
-	    pageTitle.foreach(function (sectionTitle) {
-			const hiddenElementId = this.dataset.hiddenElementId;
+		const pageTitle = document.querySelectorAll('h3[data-hidden-element-id]');
+		
+	    pageTitle.foreach(title => {
+			const hiddenElementId = title.getAttribute('data-hidden-element-id');
 			const hiddenElement = documrnt.getElementById(hiddenElementId);
-			if(hiddenElement){
+			pageTitle.addEventListener('click', function (){
 				hiddenElement.classList.toggle('hidden');
-			}
+			}};
 		});
-	});		
-   }
+	}
 });
