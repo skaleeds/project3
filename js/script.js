88956184378c9053c162f85854b3ffff5acf5eb5
skaleeds <skaleel@hawk.iit.edu>
2023-11-27 06:28:09 -0600

    changes to js and html to make all work when click
    more changes to make all work when clicked

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function(){
 	if(document.body.classList.contains('index-page')){
 		const pageTitle = document.querySelectorAll('h3[data-hidden-element-id]');
 		
-	    pageTitle.foreach(title => {
-			const hiddenElementId = title.getAttribute('data-hidden-element-id');
+	    pageTitle.forEach(currentTitle => {
+			const hiddenElementId = currentTitle.getAttribute('data-hidden-element-id');
 			const hiddenElement = documrnt.getElementById(hiddenElementId);
-			pageTitle.addEventListener('click', function (){
+			currentTitle.addEventListener('click', function (){
 				hiddenElement.classList.toggle('hidden');
 			}};
 		});
