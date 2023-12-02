8579f34573d9a5717d460a038449c1f0bb221dd2
skaleeds <skaleel@hawk.iit.edu>
2023-12-01 19:59:06 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-
 document.addEventListener('DOMContentLoaded', function(){
-    if(document.body.classList.contains('index-page')){
-        const pageTitle = document.querySelectorAll('h3[data-hidden-element-id]');
-        
-        pageTitle.forEach(title => {
-            const hiddenElementId = title.getAttribute('data-hidden-element-id');
-            const hiddenElement = document.getElementById(hiddenElementId);
+  if(document.body.classList.contains('index-page')){
+    const pageTitle = document.querySelectorAll('h3[data-hidden-element-id]');  
+      pageTitle.forEach(title => {
+        const hiddenElementId = title.getAttribute('data-hidden-element-id');
+        const hiddenElement = document.getElementById(hiddenElementId);
             
-            title.addEventListener('click', function (){
-                hiddenElement.classList.toggle('hidden');
-				title.classList.toggle('clicked');
-            });
-        });
+          title.addEventListener('click', function (){
+            hiddenElement.classList.toggle('hidden');
+	        title.classList.toggle('clicked');
+          });
+      });
     }
-	$(document).ready(function(){
-		$('.slider').slick({
-		  autoplay: true,
-		  autoplaySpeed: 1800,
-	});
+
+    $(document).ready(function(){
+      $('.slider').slick({
+        autoplay: true,
+          autoplaySpeed: 1800,
+      });
+  });
 });
