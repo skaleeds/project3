d84e13adb66e8fdfa86e22eea49e9dfba32f32e8
skaleeds <skaleel@hawk.iit.edu>
2023-12-05 02:50:39 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,29 +20,18 @@ document.addEventListener('DOMContentLoaded', function(){
   });
   
   window.initMap = function () {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: 41.8314923331667, lng: -87.62722848123455 },
-	  zoom: 8,
-    });
+	  var map = new google.maps.Map(document.getElementById('map'), {
+		  center: { lat: 41.8314923331667, lng: -87.62722848123455 },
+		  zoom: 8,
+        });
 	
-    var bounceLogo = new google.maps.Marker({
-      position: { lat: 41.8314923331667, lng: -87.62722848123455 },
-	  map: map,
-	  animation: google.maps.Animation.BOUNCE,
-	  icon: 'media/marker.png',
-	});
-	
-	var infoBox = new google.maps.InfoBox({
-		content: '<div>This is the location of the IIT Tower.</div>',
-	});
-	
-	bounceLogo.addListener('click', function () {
-		infoBox.open(map, bounceLogo);
-	});
-	
-    google.maps.event.addListener(infoBox, 'domready', function() {
-      var xBtn = document.getElementById('info-box-close');
-      xBtn.addEventListener('click', function () {
-        infoBox.close();
-    });
+  
+  var script = document.createElement('script');
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBM1F-BpX660SBK3-Vw9O9f7AEvTAIH8rI&callback=initMap';
+  script.defer = true;
+  script.async = true;
+  document.head.appendChild(script);
+  
+  var mapIframe = document.getElementById('map-iframe');
+	  mapIframe.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBM1F-BpX660SBK3-Vw9O9f7AEvTAIH8rI&q=IIT+Tower+Chicago&center=41.8314923331667,-87.62722848123455";
 });
