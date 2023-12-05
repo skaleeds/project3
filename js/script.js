676eb5fc3b0ee486d58254cdf20ac345df34d7d7
skaleeds <skaleel@hawk.iit.edu>
2023-12-05 01:32:35 -0600

    css iframe and js
    fix iframe into css

--- js/script.js
+++ js/script.js
@@ -18,11 +18,15 @@ document.addEventListener('DOMContentLoaded', function(){
           autoplaySpeed: 1800,
       });
   });
-});
-var map;
-function initMap() {
+  
+  var mapIframe = document.getElementById('map');
+	  mapIframe.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBM1F-BpX660SBK3-Vw9O9f7AEvTAIH8rI&q=IIT+Tower+Chicago&center=41.8314923331667,-87.62722848123455";
+	  
+  var map;
+  function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 41.8314923331667, lng: -87.62722848123455 },
     zoom: 8
   });
-}
+ }
+});
