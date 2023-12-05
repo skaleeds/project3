0cdfe2fdcf145feeedd8475db9a1e0b5da464350
skaleeds <skaleel@hawk.iit.edu>
2023-12-05 03:03:07 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
-document.addEventListener('DOMContentLoaded', function(){
-  if(document.body.classList.contains('index-page')){
-    const pageTitle = document.querySelectorAll('h3[data-hidden-element-id]');  
-      pageTitle.forEach(title => {
-        const hiddenElementId = title.getAttribute('data-hidden-element-id');
-        const hiddenElement = document.getElementById(hiddenElementId);
-            
-          title.addEventListener('click', function (){
-            hiddenElement.classList.toggle('hidden');
-	        title.classList.toggle('clicked');
-          });
-      });
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.body.classList.contains('index-page')) {
+    const pageTitle = document.querySelectorAll('h3[data-hidden-element-id]');
+    pageTitle.forEach(title => {
+      const hiddenElementId = title.getAttribute('data-hidden-element-id');
+      const hiddenElement = document.getElementById(hiddenElementId);
 
-    $(document).ready(function(){
-      $('.slider').slick({
-        autoplay: true,
-          autoplaySpeed: 1800,
+      title.addEventListener('click', function () {
+        hiddenElement.classList.toggle('hidden');
+        title.classList.toggle('clicked');
       });
+    });
+  }
+
+  $(document).ready(function () {
+    $('.slider').slick({
+      autoplay: true,
+      autoplaySpeed: 1800,
+    });
   });
-  
+
   window.initMap = function () {
-	  var map = new google.maps.Map(document.getElementById('map'), {
-		  center: { lat: 41.8314923331667, lng: -87.62722848123455 },
-		  zoom: 8,
-        });
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 41.8314923331667, lng: -87.62722848123455 },
+      zoom: 8,
+    });
   };
-  
+
   var script = document.createElement('script');
   script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBM1F-BpX660SBK3-Vw9O9f7AEvTAIH8rI&callback=initMap';
   script.defer = true;
   script.async = true;
   document.head.appendChild(script);
-  
+
   var mapIframe = document.getElementById('map-iframe');
-	  mapIframe.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBM1F-BpX660SBK3-Vw9O9f7AEvTAIH8rI&q=IIT+Tower+Chicago&center=41.8314923331667,-87.62722848123455";
+  mapIframe.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBM1F-BpX660SBK3-Vw9O9f7AEvTAIH8rI&q=IIT+Tower+Chicago&center=41.8314923331667,-87.62722848123455";
 });
